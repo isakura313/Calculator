afa7eecb26d51f721c4edef8ca3bfca68abed324
Paul <isakura313@gmail.com>
2020-07-23 22:15:44 +0300

    Initial Commit

---
+++ Calculator2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator2
+//
+//  Created by Павел Якупов on 23.07.2020.
+//  Copyright © 2020 Павел Якупов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
