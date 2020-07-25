de0b5cc4b5180e71b7bd61da4b0a78dccb9edbd1
Paul <isakura313@gmail.com>
2020-07-26 02:48:43 +0300

    комику только с багами

--- Calculator2/ViewController.swift
+++ Calculator2/ViewController.swift
@@ -2,197 +2,208 @@
 //  ViewController.swift
 //  Calculator
 //
-//  Created by Павел Якупов on 23.07.2020.
-//  Copyright © 2020 Павел Якупов. All rights reserved.
+//  Created by Afraz Siddiqui on 2/8/20.
+//  Copyright © 2020 ASN GROUP LLC. All rights reserved.
 //
 
 import UIKit
 
- class ViewController: UIViewController {
-    
-  
-    
+class ViewController: UIViewController {
+
     @IBOutlet var holder: UIView!
-    
+
     var firstNumber = 0
     var resultNumber = 0
     var currentOperations: Operation?
-    
-    enum Operation{
-        case add, substract, multiply, divide
+
+    enum Operation {
+        case add, subtract, multiply, divide
     }
-    
-    private var resultLabel:UILabel = {
+
+    private var resultLabel: UILabel = {
         let label = UILabel()
         label.text = "0"
         label.textColor = .white
         label.textAlignment = .right
-        label.font = UIFont(name: "Times New Roman", size: 60)
+        label.font = UIFont(name: "Helvetica", size: 100)
         return label
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Здесь у нас происходит загрузка нашего view
+        // Do any additional setup after loading the view.
     }
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-            
+
         setupNumberPad()
     }
-    private func setupNumberPad(){
+
+    private func setupNumberPad() {
         let  FontSize:CGFloat = 25
-        // здесь у нас будет функция создания наших кнопок
-        // каждая из которых занимает около четверти размера
-        let buttonSize:CGFloat = view.frame.size.width / 4
-        
-        let zeroButton = UIButton(frame: CGRect(x: 0, y:holder.frame.size.height - buttonSize, width:buttonSize * 3, height: buttonSize ))
+        let buttonSize: CGFloat = view.frame.size.width / 4
+
+        let zeroButton = UIButton(frame: CGRect(x: 0, y: holder.frame.size.height-buttonSize, width: buttonSize*3, height: buttonSize))
         zeroButton.setTitleColor(.black, for: .normal)
         zeroButton.backgroundColor = .white
         zeroButton.setTitle("0", for: .normal)
         zeroButton.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize)
         zeroButton.tag = 1
-        zeroButton.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
-
         holder.addSubview(zeroButton)
-        
-        
-        //давайте дальше воспользуемся for loop, для того, что отрисовать остальные наши кнопки
-        
-        for x in 0..<3{
-            let button1 = UIButton(frame: CGRect(x: buttonSize * CGFloat(x), y:holder.frame.size.height - (buttonSize*2), width:buttonSize , height: buttonSize ))
-             button1.setTitleColor(.black, for: .normal)
-             button1.backgroundColor = .white
-             button1.setTitle("\(x+1)", for: .normal)
-             button1.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize)
-             holder.addSubview(button1)
-             button1.tag = x + 2
-             button1.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
-             
-            
+        zeroButton.addTarget(self, action: #selector(zeroTapped), for: .touchUpInside)
+
+        for x in 0..<3 {
+            let button1 = UIButton(frame: CGRect(x: buttonSize * CGFloat(x), y: holder.frame.size.height-(buttonSize*2), width: buttonSize, height: buttonSize))
+            button1.setTitleColor(.black, for: .normal)
+            button1.backgroundColor = .white
+            button1.setTitle("\(x+1)", for: .normal)
+            holder.addSubview(button1)
+            button1.tag = x+2
+            button1.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
         }
-    
-        for x in 0..<3{
-            let button2 = UIButton(frame: CGRect(x: buttonSize * CGFloat(x), y:holder.frame.size.height - (buttonSize*3), width:buttonSize , height: buttonSize ))
-             button2.setTitleColor(.black, for: .normal)
-             button2.backgroundColor = .white
-             button2.setTitle("\(x+4)", for: .normal)
-             button2.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize)
-             holder.addSubview(button2)
-             button2.tag = x + 5
-             button2.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
+
+        for x in 0..<3 {
+            let button2 = UIButton(frame: CGRect(x: buttonSize * CGFloat(x), y: holder.frame.size.height-(buttonSize*3), width: buttonSize, height: buttonSize))
+            button2.setTitleColor(.black, for: .normal)
+            button2.backgroundColor = .white
+            button2.setTitle("\(x+4)", for: .normal)
+            holder.addSubview(button2)
+            button2.tag = x+5
+            button2.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
         }
-        
-        for x in 0..<3{
-            let button3 = UIButton(frame: CGRect(x: buttonSize * CGFloat(x), y:holder.frame.size.height - (buttonSize*4), width:buttonSize , height: buttonSize ))
-             button3.setTitleColor(.black, for: .normal)
-             button3.backgroundColor = .white
-             button3.setTitle("\(x+7)", for: .normal)
-            button3.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize)
-             holder.addSubview(button3)
-            button3.tag = x + 8
+
+        for x in 0..<3 {
+            let button3 = UIButton(frame: CGRect(x: buttonSize * CGFloat(x), y: holder.frame.size.height-(buttonSize*4), width: buttonSize, height: buttonSize))
+            button3.setTitleColor(.black, for: .normal)
+            button3.backgroundColor = .white
+            button3.setTitle("\(x+7)", for: .normal)
+            holder.addSubview(button3)
+            button3.tag = x+8
             button3.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
         }
-        //дальше давайте сделаем клавишу, которая у нас будет все выключать, если что:
-        let clearButton = UIButton(frame: CGRect(x: 0, y:holder.frame.size.height - (buttonSize*5), width:view.frame.size.width - buttonSize , height: buttonSize ))
-         clearButton.setTitleColor(.white, for: .normal)
-         clearButton.backgroundColor = .blue
-         clearButton.setTitle("Очистить", for: .normal)
-         clearButton.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize)
-         holder.addSubview(clearButton)
 
-          let operations = ["= ","+", "-", "*", "÷"]
-        
-        for x in 0..<5{
-            let button_operand = UIButton(frame: CGRect(x: buttonSize * 3, y:holder.frame.size.height - (buttonSize*CGFloat(x+1)), width:buttonSize , height: buttonSize ))
-             button_operand.setTitleColor(.white, for: .normal)
-            button_operand.backgroundColor = .purple
-             button_operand.setTitle(operations[x], for: .normal)
-            button_operand.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize+3)
-             holder.addSubview(button_operand)
+        let clearButton = UIButton(frame: CGRect(x: 0, y: holder.frame.size.height-(buttonSize*5), width: view.frame.size.width - buttonSize, height: buttonSize))
+        clearButton.setTitleColor(.black, for: .normal)
+        clearButton.backgroundColor = .white
+        clearButton.setTitle("CE", for: .normal)
+        holder.addSubview(clearButton)
+
+
+        let operations = ["=","+", "-", "x", "/"]
+
+        for x in 0..<5 {
+            let button_operand = UIButton(frame: CGRect(x: buttonSize * 3, y: holder.frame.size.height-(buttonSize * CGFloat(x+1)), width: buttonSize, height: buttonSize))
+            button_operand.setTitleColor(.black, for: .normal)
+            button_operand.backgroundColor = .init(red: 2, green: 0.8, blue: 0, alpha: 1)
+            button_operand.setTitle(operations[x], for: .normal)
+            holder.addSubview(button_operand)
             button_operand.tag = x+1
-            button_operand.addTarget(self,  action: #selector(operationPressed(_:)), for: .touchUpInside )
+            button_operand.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize)
+            button_operand.addTarget(self, action: #selector(operationPressed(_:)), for: .touchUpInside)
         }
-        
-        
+
         resultLabel.frame = CGRect(x: 20, y: clearButton.frame.origin.y - 110.0, width: view.frame.size.width - 40, height: 100)
         holder.addSubview(resultLabel)
-        
-        //добавляем действия к нашей кнопке очистить
-        clearButton.addTarget(self , action: #selector(clearResult), for: .touchUpInside)
-        
+
+        // Actions
+        clearButton.addTarget(self, action: #selector(clearResult), for: .touchUpInside)
     }
-    
-    @objc func clearResult(){
+
+    @objc func clearResult() {
         resultLabel.text = "0"
-        currentOperations = nil // в результате мы хот
-        
+        currentOperations = nil
+        firstNumber = 0
     }
-    
-    @objc func numberPressed(_ sender: UIButton){
+
+    @objc func zeroTapped() {
+
+        if resultLabel.text != "0" {
+            if let text = resultLabel.text {
+                resultLabel.text = "\(text)\(0)"
+            }
+        }
+    }
+
+
+    @objc func numberPressed(_ sender: UIButton) {
         let tag = sender.tag - 1
-        
-        if resultLabel.text == "0"{
+
+        if resultLabel.text == "0" {
             resultLabel.text = "\(tag)"
-        }else if let text = resultLabel.text{
+        }
+        else if let text = resultLabel.text {
             resultLabel.text = "\(text)\(tag)"
         }
-        
     }
-    
-    @objc func operationPressed(_ sender: UIButton){
+
+    @objc func operationPressed(_ sender: UIButton) {
         let tag = sender.tag
-        if let text = resultLabel.text, let value = Int(text), firstNumber == 0{
-             firstNumber = value
-            resultLabel.text  = "0"
+
+        if let text = resultLabel.text, let value = Int(text), firstNumber == 0 {
+            firstNumber = value
+            resultLabel.text = "0"
         }
-       
-        
-        if tag == 1{
-            if let operation = currentOperations{
+
+        if tag == 1 {
+            if let operation = currentOperations {
                 var secondNumber = 0
-                if let text = resultLabel.text, let value = Int(text){
+                if let text = resultLabel.text, let value = Int(text) {
                     secondNumber = value
                 }
-                switch operation{
+
+                switch operation {
                 case .add:
+
+                    firstNumber = firstNumber + secondNumber
+                    secondNumber = 0
+                    resultLabel.text = "\(firstNumber)"
+                    currentOperations = nil
+                    firstNumber = 0
                     
-                    let result = firstNumber  + secondNumber
-                    resultLabel.text = "\(result)"
                     break
-                case .substract:
+
+                case .subtract:
+                    firstNumber = firstNumber - secondNumber
+                    secondNumber = 0
+                    resultLabel.text = "\(firstNumber)"
+                    currentOperations = nil
+                    firstNumber = 0
                     
-                    let result = firstNumber  - secondNumber
-                    resultLabel.text = "\(result)"
                     break
+
                 case .multiply:
-                    
-                    let result = firstNumber  * secondNumber
-                    resultLabel.text = "\(result)"
+                    firstNumber = firstNumber * secondNumber
+                    secondNumber = 0
+                    resultLabel.text = "\(firstNumber)"
+                    currentOperations = nil
+                    firstNumber = 0
+
                     break
+
                 case .divide:
-                    
-                    let result = firstNumber  / secondNumber
-                    resultLabel.text = "\(result)"
+                    firstNumber = firstNumber / secondNumber
+                    secondNumber = 0
+                    resultLabel.text = "\(firstNumber)"
+                    currentOperations = nil
+                    firstNumber = 0
                     break
                 }
             }
         }
-        else if tag == 2{
+        else if tag == 2 {
             currentOperations = .add
         }
-        else if tag == 3{
-            currentOperations = .substract
+        else if tag == 3 {
+            currentOperations = .subtract
         }
-        else if tag == 4{
+        else if tag == 4 {
             currentOperations = .multiply
         }
-        else if tag == 5{
+        else if tag == 5 {
             currentOperations = .divide
         }
-        
 
-        
     }
-}
 
+}
