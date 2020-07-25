8fd63294b4183025e20dcfae668f10fa600e4361
Paul <isakura313@gmail.com>
2020-07-25 23:27:49 +0300

    asdasd

--- Calculator2/ViewController.swift
+++ Calculator2/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Calculator2
+//  Calculator
 //
 //  Created by Павел Якупов on 23.07.2020.
 //  Copyright © 2020 Павел Якупов. All rights reserved.
@@ -13,23 +13,31 @@ import UIKit
   
     
     @IBOutlet var holder: UIView!
+    
+    var firstNumber = 0
+    var resultNumber = 0
+    var currentOperations: Operation?
+    
+    enum Operation{
+        case add, substract, multiply, divide
+    }
+    
     private var resultLabel:UILabel = {
         let label = UILabel()
         label.text = "0"
         label.textColor = .white
         label.textAlignment = .right
-        label.font = UIFont(name: "Times New Roman", size: 80)
+        label.font = UIFont(name: "Times New Roman", size: 60)
         return label
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Здесь у нас происходит загрузка нашего view
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
-        
+            
         setupNumberPad()
     }
     private func setupNumberPad(){
@@ -43,6 +51,9 @@ import UIKit
         zeroButton.backgroundColor = .white
         zeroButton.setTitle("0", for: .normal)
         zeroButton.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize)
+        zeroButton.tag = 1
+        zeroButton.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
+
         holder.addSubview(zeroButton)
         
         
@@ -55,6 +66,8 @@ import UIKit
              button1.setTitle("\(x+1)", for: .normal)
              button1.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize)
              holder.addSubview(button1)
+             button1.tag = x + 2
+             button1.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
              
             
         }
@@ -66,6 +79,8 @@ import UIKit
              button2.setTitle("\(x+4)", for: .normal)
              button2.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize)
              holder.addSubview(button2)
+             button2.tag = x + 5
+             button2.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
         }
         
         for x in 0..<3{
@@ -75,6 +90,8 @@ import UIKit
              button3.setTitle("\(x+7)", for: .normal)
             button3.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize)
              holder.addSubview(button3)
+            button3.tag = x + 8
+            button3.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
         }
         //дальше давайте сделаем клавишу, которая у нас будет все выключать, если что:
         let clearButton = UIButton(frame: CGRect(x: 0, y:holder.frame.size.height - (buttonSize*5), width:view.frame.size.width - buttonSize , height: buttonSize ))
@@ -93,6 +110,8 @@ import UIKit
              button_operand.setTitle(operations[x], for: .normal)
             button_operand.titleLabel?.font = UIFont(name: "Helvetica", size: FontSize+3)
              holder.addSubview(button_operand)
+            button_operand.tag = x+1
+            button_operand.addTarget(self,  action: #selector(operationPressed(_:)), for: .touchUpInside )
         }
         
         
@@ -106,6 +125,74 @@ import UIKit
     
     @objc func clearResult(){
         resultLabel.text = "0"
+        currentOperations = nil // в результате мы хот
+        
+    }
+    
+    @objc func numberPressed(_ sender: UIButton){
+        let tag = sender.tag - 1
+        
+        if resultLabel.text == "0"{
+            resultLabel.text = "\(tag)"
+        }else if let text = resultLabel.text{
+            resultLabel.text = "\(text)\(tag)"
+        }
+        
+    }
+    
+    @objc func operationPressed(_ sender: UIButton){
+        let tag = sender.tag
+        if let text = resultLabel.text, let value = Int(text), firstNumber == 0{
+             firstNumber = value
+            resultLabel.text  = "0"
+        }
+       
+        
+        if tag == 1{
+            if let operation = currentOperations{
+                var secondNumber = 0
+                if let text = resultLabel.text, let value = Int(text){
+                    secondNumber = value
+                }
+                switch operation{
+                case .add:
+                    
+                    let result = firstNumber  + secondNumber
+                    resultLabel.text = "\(result)"
+                    break
+                case .substract:
+                    
+                    let result = firstNumber  - secondNumber
+                    resultLabel.text = "\(result)"
+                    break
+                case .multiply:
+                    
+                    let result = firstNumber  * secondNumber
+                    resultLabel.text = "\(result)"
+                    break
+                case .divide:
+                    
+                    let result = firstNumber  / secondNumber
+                    resultLabel.text = "\(result)"
+                    break
+                }
+            }
+        }
+        else if tag == 2{
+            currentOperations = .add
+        }
+        else if tag == 3{
+            currentOperations = .substract
+        }
+        else if tag == 4{
+            currentOperations = .multiply
+        }
+        else if tag == 5{
+            currentOperations = .divide
+        }
+        
+
+        
     }
 }
 
